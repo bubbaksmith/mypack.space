cbb9c45d7e43cbe9b86ff7a9bb48a177c17c968e
Brian Smith <brian.smith.keith@gmail.com>
2016-04-27 22:47:09 -0400

    playing with bootstrap

--- mypack/config/routes.rb
+++ mypack/config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root to: 'pages#home'
+  root to: 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'brian' => 'pages#brian'
+  get 'brian' => 'pages#home'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
